573d794463cd0bcc98ed524f79146f8d934ff65e
Jeff Hale <discdiver@users.noreply.github.com>
2019-03-31 10:02:43 -0400

    update spacing

--- script.py
+++ script.py
@@ -13,40 +13,43 @@ with open ('./data/genderwords.csv') as words:
         with open('./data/Harriet.txt', 'w') as f2:
             for line in f1:
                 x = line
+                
                 # iterate through the dictionary on that one line making any changes necessary for individual words
                 for oldword, newword in mydict.items():
                     regex_match = r'\b(%s)+\b'
                     reg = regex_match % oldword
                     x = re.sub(reg, oldword+'silly', x)
                 f2.write(x)
-            f2.close()
-        f1.close()
-        with open('./data/Harriet.txt', 'r') as f4:
-            with open('./data/final.txt', 'w') as f3:
-                for line2 in f4:
-                    x2 = line2
-                    for oldword, newword in mydict.items():
-                        regex_match = r'\b(%s)+\b'
-                        sil = oldword+'silly'
-                        reg = regex_match % sil
-                        x2 = re.sub(reg, newword, x2)
-                    f3.write(x2)
-                f3.close()
-            f4.close()
+         f2.close()
+     f1.close()
+        
+     with open('./data/Harriet.txt', 'r') as f4:
+        with open('./data/final.txt', 'w') as f3:
+            for line2 in f4:
+                x2 = line2
+                for oldword, newword in mydict.items():
+                    regex_match = r'\b(%s)+\b'
+                    sil = oldword+'silly'
+                    reg = regex_match % sil
+                    x2 = re.sub(reg, newword, x2)
+                f3.write(x2)
+         f3.close()
+     f4.close()
 words.close()
 
 # her can correspond with him or his.
 # same with his and her or hers.
-# need to figuore out by part of speech
+# need to figure out by part of speech
 
-#x = line.replace(" "+oldword+" ", " "+oldword+"silly")
+# x = line.replace(" "+oldword+" ", " "+oldword+"silly")
 # then  f2.write(line.replace(" "+oldword+"silly", " "+neword+" ")
 
-'''# script to read in file, replace words, and output new file
+''' 
+# script to read in file, replace words, and output new file
 import re
 import pandas as pd
-# read in file
 
+# read in file
 gdf = pd.read_csv('./data/genderwords.csv')     # import list of genderedword pairs as a dataframe
 print(gdf)
 df = gdf.loc[:, ('froms', 'tos')]
@@ -66,5 +69,6 @@ with open('./data/Harriet.txt', 'r') as f:
         newtxt = re.sub(word, sword+'ooyt ', content)
         content = newtxt
     print(content[:550])
+    
 # output the new text into a new file
 '''
